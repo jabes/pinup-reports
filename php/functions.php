4eedd353bda90d05f3287eefe16ea66da8fc3d93
Justin Bull <bull.justin@gmail.com>
2017-09-22 08:15:35 -0700

    Remove JSON shim which has been baked into PHP for some time now... how old is this

--- php/functions.php
+++ php/functions.php
@@ -1,33 +1,5 @@
 <?php
 
-
-if (!function_exists('json_encode')) {
-	require_once dirname(__FILE__) . "/../JSON/JSON.php";
-	function json_encode($arg)
-	{
-		global $services_json;
-		if (!isset($services_json)) {
-			$services_json = new Services_JSON();
-		}
-		return $services_json->encode($arg);
-	}
-}
-
-if (!function_exists('json_decode')) {
-	require_once dirname(__FILE__) . "/../JSON/JSON.php";
-	function json_decode($arg)
-	{
-		global $services_json;
-		if (!isset($services_json)) {
-			$services_json = new Services_JSON();
-		}
-		return $services_json->decode($arg);
-	}
-} 
-
-
-
-
 function checkMem($name) {
 	global $memcache;
 	if ($memcache && $name) {

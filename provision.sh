8c5a1aa86a82392d8ebfc4dc7bd19d30be47ae8a
Justin Bull <bull.justin@gmail.com>
2017-09-22 11:12:00 -0700

    Install php mcrypt

--- provision.sh
+++ provision.sh
@@ -7,13 +7,16 @@ sudo apt-get -y update
 # This project is old.. and is not able to use the mysqli extension
 # So downgrade to php5.6
 sudo apt-get -y purge php.*
+sudo apt-get -y autoremove
 sudo apt-get -y install php5.6 \
                         php5.6-common \
                         php5.6-curl \
                         php5.6-json \
                         php5.6-mbstring \
+                        php5.6-mcrypt \
                         php5.6-memcache \
                         php5.6-mysql \
                         libapache2-mod-php5.6
-sudo apt-get -y autoremove
+sudo php5enmod mcrypt
+sudo php5enmod memcache
 sudo service apache2 restart
